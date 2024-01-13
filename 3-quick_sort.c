7d30108bcf7eac4bdc260e5479e2ebf1f5c8d9f4
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-13 19:23:30 +0100

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,12 +1,48 @@
 #include "sort.h"
 /**
- * quick_sort - Sorts an array of integers in ascending order using Quick Sort.
- * @array: The array to be sorted.
- * @size: The size of the array.
+ * swap - Swaps the values of two integers.
+ * @i: Pointer to the first integer.
+ * @j: Pointer to the second integer.
+ *
+ * This function takes two integer pointers as input and swaps
+ * the values they point to.
  */
-void quick_sort(int *array, size_t size)
+void swap(int *i, int *j)
 {
-	quick_sort_recursion(array, 0, size - 1, size);
+	int tmp = *i;
+	*i = *j;
+	*j = tmp;
+}
+
+/**
+ * partition - Partitions the array and returns the pivot index.
+ * @array: The array to be partitioned.
+ * @low: The starting index of the array or subarray.
+ * @high: The ending index of the array or subarray.
+ * @size: The size of the array.
+ * Return: The index of the pivot element.
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot_value = array[high];
+	int i = low, j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot_value)
+		{
+			if (i < j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+
+	swap(&array[i], &array[high]);
+	print_array(array, size);
+	return (i);
 }
 /**
  * quick_sort_recursion - Recursively sorts an array
@@ -18,43 +54,20 @@ void quick_sort(int *array, size_t size)
  */
 void quick_sort_recursion(int *array, int low, int high, size_t size)
 {
-	if (low < high)
-	{
-		int pivot_index = partition(array, low, high);
+	if (low >= high)
+		return;
+	int pivot_index = partition(array, low, high, size);
 
-		print_array(array, size);
-		quick_sort_recursion(array, low, pivot_index - 1, size);
-		quick_sort_recursion(array, pivot_index + 1, high, size);
-	}
+	quick_sort_recursion(array, low, pivot_index - 1, size);
+	quick_sort_recursion(array, pivot_index + 1, high, size);
 }
 
 /**
- * partition - Partitions the array and returns the pivot index.
- * @array: The array to be partitioned.
- * @low: The starting index of the array or subarray.
- * @high: The ending index of the array or subarray.
- * Return: The index of the pivot element.
+ * quick_sort - Sorts an array of integers in ascending order using Quick Sort.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
  */
-int partition(int *array, int low, int high)
+void quick_sort(int *array, size_t size)
 {
-	int pivot_value = array[high];
-	int i = low, j;
-	int tmp;
-
-	for (j = low; j < high; j++)
-	{
-		if (array[j] <= pivot_value)
-		{
-			tmp = array[i];
-			array[i] = array[j];
-			array[j] = tmp;
-			i++;
-		}
-	}
-
-	tmp = array[i];
-	array[i] = array[high];
-	array[high] = tmp;
-
-	return (i);
+	quick_sort_recursion(array, 0, size - 1, size);
 }
